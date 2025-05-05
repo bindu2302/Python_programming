1503b5a3ca155c1c345ef51955cf49b439cfc9f8
HimaBindu Nara <naarahimabindu@gmail.com>
2025-05-05 21:02:36 +0530

    Update decorator.py

--- Advance_Python/decorator.py
+++ Advance_Python/decorator.py
@@ -1,3 +1,14 @@
+'''
+Decorator: A function in python that takes another function as input and add something extra to it, like layer of frosting.
+it's like a wrapping a function inside a package to make it even better.Decorator helps you to add features like
+logging, checking,timing without rewriting the original code.
+
+@symbol: is used to easily apply a decorator to a function.
+wrapper fucntion: The fucntion inside the decorator that add the extra functionality. 
+
+ '''
+
+
 def decor(func):
     def inner(name):
         if name == "Ayush":
@@ -33,15 +44,56 @@ div(10,0) #b should not be 0
 
 
 
-def repeat(num):
-    def outer(func):
-        def inner():
-            for i in range(num):
-                func()
-        return inner
-    return outer
 
-@repeat(num = 3)
-def msg():
-    print("Hello")
-msg()
+def auth_decorator(func):
+    def wrapper(username,password):
+        if username=="admin" and password=="password":
+            print("Authentication successful.")
+            return func()
+        else:
+            print("Unauthorized")
+    return wrapper
+
+@auth_decorator
+def secure_func():
+    print("Hello, authenticated user!")
+secure_func("admin","password")
+
+
+# Authentication successful.
+# Hello, authenticated user!
+
+
+def logger_decorator(func):
+    def wrapper():
+        print("Starting function execution...")
+        func()
+        print("Function execution complete.")
+    return wrapper
+
+@logger_decorator
+def say_hello():
+    print("Hello, world!")
+say_hello()
+
+# Starting function execution...
+# Hello, world!
+# Function execution complete.
+
+
+def repeat_decorator(times):
+    def decorator(func):
+        def wrapper():
+            for i in range(times):
+                func()
+        return wrapper
+    return decorator
+
+@repeat_decorator(times=3)
+def say_hello():
+    print("Hello, world!")
+say_hello()
+
+# Hello, world!
+# Hello, world!
+# Hello, world!
